00cb39d037ff0a70887d540ea3e412e2dead1cad
gyan <gyan9427@gmail.com>
2021-08-19 18:27:37 +0530

    add construct to personController.php

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -3,3 +3,20 @@ namespace Src\Controller;
 
 use Src\TableGateways\PersonGateway;
 
+class PersonController{
+    private $db;
+    private $requestMethod;
+    private $userId;
+
+    private $personGateway;
+
+    public function __construct($db,$requestMethod,$userId){
+        $this->db = $db;
+        $this->requestMethod =$requestMethod;
+        $this->userId = $userId;
+
+        $this->personGateway = new PersonGateway($db);
+    }
+
+    
+}
